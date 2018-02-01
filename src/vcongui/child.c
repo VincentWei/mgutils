3c5711614df503621a62677b6827090f1b56f4b6
VincentWei <vincent@minigui.org>
2018-02-01 18:06:49 +0800

    add GVPv3 copying info to source files

--- src/vcongui/child.c
+++ src/vcongui/child.c
@@ -1,33 +1,34 @@
 /*
-** $Id: child.c 231 2017-10-26 07:25:36Z weiym $
-**
-** KON2 - Kanji ON Console -
-** Copyright (C) 1992-1996 Takashi MANABE (manabe@papilio.tutics.tut.ac.jp)
-**
-** CCE - Console Chinese Environment -
-** Copyright (C) 1998-1999 Rui He (herui@cs.duke.edu)
-**
-** VCOnGUI - Virtual Console On MiniGUi -
-** Copyright (C) 1999-2002 Wei Yongming (ymwei@minigui.org)
-** Copyright (C) 2003-2007 Feynman Software
-**
-*/
-
-/*
-** This program is free software; you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 2 of the License, or
-** (at your option) any later version.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-** GNU General Public License for more details.
-**
-** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software
-** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ *   This file is part of mGUtils, a component for MiniGUI.
+ * 
+ *   Copyright (C) 2003~2018, Beijing FMSoft Technologies Co., Ltd.
+ *   Copyright (C) 1998~2002, WEI Yongming
+ * 
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *   Or,
+ * 
+ *   As this program is a library, any link to this program must follow
+ *   GNU General Public License version 3 (GPLv3). If you cannot accept
+ *   GPLv3, you need to be licensed from FMSoft.
+ * 
+ *   If you have got a commercial license of this program, please use it
+ *   under the terms and conditions of the commercial license.
+ * 
+ *   For more information about the commercial license, please refer to
+ *   <http://www.minigui.com/en/about/licensing-policy/>.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
--- src/vcongui/paint.h
+++ src/vcongui/paint.h
@@ -1,30 +1,34 @@
 /*
- * $Id: paint.h 1 2007-12-28 06:28:48Z xwyan $
- *
- * paint.h: window painting
+ *   This file is part of mGUtils, a component for MiniGUI.
  * 
- * VCOnGUI - Virtual Console On MiniGUi -
- * Copyright (c) 1999-2002 Wei Yongming (ymwei@minigui.org)
- * Copyright (C) 2003-2007 Feynman Software
- *
+ *   Copyright (C) 2003~2018, Beijing FMSoft Technologies Co., Ltd.
+ *   Copyright (C) 1998~2002, WEI Yongming
+ * 
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *   Or,
+ * 
+ *   As this program is a library, any link to this program must follow
+ *   GNU General Public License version 3 (GPLv3). If you cannot accept
+ *   GPLv3, you need to be licensed from FMSoft.
+ * 
+ *   If you have got a commercial license of this program, please use it
+ *   under the terms and conditions of the commercial license.
+ * 
+ *   For more information about the commercial license, please refer to
+ *   <http://www.minigui.com/en/about/licensing-policy/>.
  */
-
-/*
-**  This library is free software; you can redistribute it and/or
-**  modify it under the terms of the GNU Library General Public
-**  License as published by the Free Software Foundation; either
-**  version 2 of the License, or (at your option) any later version.
-**
-**  This software is distributed in the hope that it will be useful,
-**  but WITHOUT ANY WARRANTY; without even the implied warranty of
-**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-**  Library General Public License for more details.
-**
-**  You should have received a copy of the GNU Library General Public
-**  License along with this library; if not, write to the Free
-**  Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-**  MA 02111-1307, USA
-*/
 
 #ifndef PAINT_VCONGUI_H
 #define PAINT_VCONGUI_H
